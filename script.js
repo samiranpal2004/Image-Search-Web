427cd80894a90dd5aef1e8ded38f3d1984da7057
Samiran Pal <palsamiranpal2004@gmail.com>
2024-09-30 01:26:52 +0530

    finalCommit

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ let keyword = "";
 let page = 1;
 async function searchImages() {
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(url);
   const data = await response.json();
+
+  if(page === 1){
+    searchResult.innerHTML = "";
+  }
 
   const results = data.results;
   results.map((result) => {
@@ -25,9 +29,14 @@ async function searchImages() {
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   });
+  showMoreBtn.style.display = "block";
 }
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
   searchImages();
 });
+showMoreBtn.addEventListener("click", ()=>{
+  page++;
+  searchImages();
+})
